31094eb83afa8a83d4858552292a949c25cb68a6
JImmyJImmy0 <jerickson317@gmail.com>
2021-08-24 10:33:38 -0600

    home and foods index working

--- main_app/views.py
+++ main_app/views.py
@@ -1,23 +1,12 @@
 from django.shortcuts import render
+from .models import Food
 
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
-class Food:
-    def __init__(self, name, protein, carbs, fat, sugar, calories, sodium):
-        self.name = name
-        self.protein = protein
-        self.carbs = carbs
-        self.fat = fat
-        self.sugar = sugar
-        self.calories = calories
-        self.sodium = sodium
-    
-foods = [
-    Food('Chip', 1, 10, 4, 2, 100, 300)
-]
 
 def foods_index(request):
+    foods = Food.objects.all()
     return render(request, 'foods/index.html', { 'foods': foods })
