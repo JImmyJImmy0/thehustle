44307e274b3dc528cbf392895e1df980b3d5f28a
JImmyJImmy0 <jerickson317@gmail.com>
2021-08-24 10:11:39 -0600

    hard code a food

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('food/', views.food_index, name='food_index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -4,3 +4,20 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return HttpResponse('<h1>Working</h1>')
+
+class Food:
+    def __init__(self, name, protein, carbs, fat, sugar, calories, sodium):
+        self.name = name
+        self.protein = protein
+        self.carbs = carbs
+        self.fat = fat
+        self.sugar = sugar
+        self.calories = calories
+        self.sodium = sodium
+    
+foods = [
+    Food('Chip', 1, 10, 10, 4, 2, 100, 300),
+]
+
+def foods_index(request):
+    return render(request, 'foods/index.html', { 'foods', foods })
