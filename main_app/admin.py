5fab6dd6f4050b474fb301e9eeb5ab0b13e4a4b9
JImmyJImmy0 <jerickson317@gmail.com>
2021-08-25 14:36:40 -0600

    add exercise to admin

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Food
+from .models import Food, Exercise
 
 # Register your models here.
 admin.site.register(Food)
+admin.site.register(Exercise)
