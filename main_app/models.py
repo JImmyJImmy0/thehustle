52660b51cba21cb45836c7f90604ad1582cafb94
JImmyJImmy0 <jerickson317@gmail.com>
2021-08-25 14:36:03 -0600

    make exercise model and migrate

--- main_app/models.py
+++ main_app/models.py
@@ -16,3 +16,13 @@ class Food(models.Model):
 
     # def get_absolute_url(self):
     #     return reverse('foods_index', kwargs={'food_id': self.id})
+
+
+class Exercise(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+    duration = models.IntegerField()
+    calories_burned = models.IntegerField()
+
+    def __str__(self):
+        return self.name
