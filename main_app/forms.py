294be3c2c89f3ebfe03d676867b4ddecd4184c02
JImmyJImmy0 <jerickson317@gmail.com>
2021-08-26 11:27:33 -0600

    make custom profile form

---
+++ main_app/forms.py
@@ -0,0 +1,8 @@
+from django.db.models import fields
+from django.forms import ModelForm
+from .models import Profile
+
+class ProfileForm(ModelForm):
+    class Meta:
+        model = Profile
+        fields = '__all__'
