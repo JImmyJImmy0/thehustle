79cb03cb7395aa41bf1d22c26f4eabb07cc3806d
JImmyJImmy0 <jerickson317@gmail.com>
2021-08-27 13:30:58 -0600

    migrate Meal and add to admin

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Food, Exercise, Profile
+from .models import Food, Exercise, Profile, Meal
 
 # Register your models here.
 admin.site.register(Food)
 admin.site.register(Exercise)
 admin.site.register(Profile)
+admin.site.register(Meal)
