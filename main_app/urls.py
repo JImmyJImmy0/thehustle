0324dbf4f421e69a61d44655b7e56aded80f45f5
JImmyJImmy0 <jerickson317@gmail.com>
2021-08-27 12:45:22 -0600

    restart, but with meal model idea

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,8 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
     path('food/', views.foods_index, name='foods_index'),
-    # path('food/', views.model_to_dict, name='model_to_dict'),
-    path('food/', views.create_dict, name='create_dict'),
     path('foodlog/', views.food_log, name='food_log'),
     path('food/create/', views.FoodCreate.as_view(), name='foods_create'),
     path('food/<int:pk>/delete/', views.FoodDelete.as_view(), name='foods_delete'),
